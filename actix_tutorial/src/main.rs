a4cecea0d2c1ae102cdd40c7e1994b83660aeab5
SamuelLJackson <severett29@gmail.com>
2020-06-16 20:47:05 -0400

    middle of second video

--- actix_tutorial/src/main.rs
+++ actix_tutorial/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+mod models;
+mod config;
+
+use crate::models::Status;
+use actix_web::{HttpServer, App, web, Responder};
+use std::io;
+use dotenv::dotenv;
+use tokio_postgres::NoTls;
+
+async fn status() -> impl Responder {
+    web::HttpResponse::Ok()
+        .json(Status{ status: String::from("Ok")})
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+
+    dotenv().ok();
+
+    let config = crate::config::Config::from_env().unwrap();
+
+    let pool = config.pg.create_pool(NoTls).unwrap();
+
+    println!("Server starting at http://{}:{}", config.server.host, config.server.port);
+
+    HttpServer::new(move || {
+        App::new()
+            .data(pool.clone())
+            .route("/", web::get().to(status))
+    })
+    .bind(format!("{}:{}", config.server.host, config.server.port)?
+    .run()
+    .await
 }
